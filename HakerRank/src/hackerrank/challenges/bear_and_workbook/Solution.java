1ff026ed36a2f44d5b8d1fd7cea7eaaa3bdc0617
bryanjimenez <bxj62@miami.edu>
2016-05-25 16:35:42 -0400

    practicing!

---
+++ HakerRank/src/hackerrank/challenges/bear_and_workbook/Solution.java
@@ -0,0 +1,43 @@
+package hackerrank.challenges.bear_and_workbook;
+
+import java.util.*;
+
+/**
+ * https://www.hackerrank.com/challenges/bear-and-workbook
+ * 
+ * @author bxj62
+ *
+ */
+public class Solution {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int t = in.nextInt();
+
+		int page = 0;
+		int special = 0;
+
+		for (int a = 0; a < n; a++) {
+			int p = in.nextInt();
+
+			page++;
+//			System.out.println("chapter "+(a+1) +" page: "+ page);
+
+			for (int i = 0; i < p; i++) {
+				if (i + 1 == page) {
+					special++;
+				}
+				if ((i + 1)%t == 0 && i+1<p) {
+					page++;
+				}
+			}
+//			System.out.println("chapter "+(a+1) +" s: "+ special);
+
+		}
+		System.out.println(special);
+		in.close();
+	}
+
+}
--- HakerRank/src/hackerrank/challenges/chocolate_feast/Solution.java
+++ HakerRank/src/hackerrank/challenges/chocolate_feast/Solution.java
@@ -1,10 +1,6 @@
 package hackerrank.challenges.chocolate_feast;
 
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 /**
  * https://www.hackerrank.com/challenges/chocolate-feast
  * @author bxj62
@@ -15,14 +11,29 @@ public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
+
+        List<Integer> out = new ArrayList<Integer>();
         for(int a0 = 0; a0 < t; a0++){
             int n = in.nextInt();
             int c = in.nextInt();
             int m = in.nextInt();
             
-            List<Integer> out = new ArrayList<Integer>();
-            
+            out.add(n/c+ chocolates(m,n/c));
             
         }
+        
+        for(int o:out){
+        	System.out.println(o);
+        }
+        in.close();
+    }
+    
+    public static int chocolates(int M, int wrappers){
+    	if(wrappers<M){
+    		return 0;
+    	}else{
+    		// wrappers from chocolates obtained free + wrappers left over
+    		return wrappers/M+chocolates(M,wrappers/M+wrappers%M);
+    	}
     }
 }
